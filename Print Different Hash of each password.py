bdb2c55a8b50e194ea7fbdfab8c1806b7adca2f2
CJ <cjperez127@gmail.com>
2024-02-10 20:50:42 -0600

    Finished basic project of PW Hash viewer

---
+++ Print Different Hash of each password.py
@@ -0,0 +1,19 @@
+#Custom hash comparison of different hashes depending on hash type E.G. SHA256, MD5, SHA3-256
+
+import hashlib
+import getpass
+
+hashlist = {} #stores key:value pairs of password : hash
+
+#def create_password():
+print("""
+          Hello, this is a program where you can view the hashes of 3 different hash types: SHA256, MD5, SHA3-256.
+          you enter your password and we'll output the 3 different hashes in 3 different lines for your password.
+          """)
+password = input("Enter the password you'd like to view the hashes of: ")
+hashed_password = hashlib.sha256(password.encode()).hexdigest()
+print("This is the hash in SHA-256\n", hashed_password)
+hashed_password2 = hashlib.md5(password.encode()).hexdigest()
+print("\nThis is the hash in MD5\n", hashed_password2)
+hashed_password3 = hashlib.sha3_256(password.encode()).hexdigest()
+print("\nThis is the hash in SHA3-256\n", hashed_password3)
